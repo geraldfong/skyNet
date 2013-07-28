06e705871b5c21e8277b2edb08422b32a721d754
Achal Dave <achalddave@live.com>
2013-07-28 03:19:27 -0700

    merge data_parser to return medians for various ssids

--- data_parser.py
+++ data_parser.py
@@ -1,63 +1,53 @@
 import json
 import math
+import pprint
 data_path = "data.json"
 
-# feet to array of strengths
-strength_data = {}
-averaged_data = {}
-sd_data = {}
+class SSIDData:
+    def __init__(self, name):
+        self.name = name
+        self.data = {}
+        self.finalized = False
 
-post_filter_averaged_data = {}
-post_filter_sd_data = {}
+    def add_data(self,ft, strength):
+        if ft not in self.data:
+            self.data[ft] = []
+        self.data[ft].append(strength)
+
+    def finalize(self):
+        for values in self.data.values():
+            values.sort()
+        self.finalized = True
+
+    def get_median_strength(self, ft):
+        if not self.finalized:
+            self.finalize()
+        values = self.data[ft]
+        return values[len(values)/2]
+
+    def get_median_strengths(self):
+        medians = {}
+        for ft in self.data.keys():
+            medians[ft] = self.get_median_strength(ft)
+        return medians
+
+ssid_datas = {}
 
 for line in open(data_path):
     try:
         data = json.loads(line)
-        ft = data['feet']
-        values = data['data'] # yes.
-        accum = 0
-        for value in values:
-            if ft not in strength_data:
-                strength_data[ft] = []
-            strength_data[ft].append(int(value['strength']))
     except ValueError:
-        print("Couldn't load line: " + line)
+        print("Couldn't read line: ")
+        print(line)
+        raise
+    ft = data['feet']
+    all_strengths = data['data'] # yes.
+    for strength_data in all_strengths:
+        ssid_name = strength_data['ssid']
+        if ssid_name not in ssid_datas:
+            ssid_datas[ssid_name] = SSIDData(ssid_name)
+        ssid_datas[ssid_name].add_data(ft, strength_data['strength'])
 
-def square(x):
-    return x * x
+medians = dict([(name, data.get_median_strengths()) for name, data in ssid_datas.items()])
 
-for ft, values in strength_data.items():
-    avg = sum(values)/len(values)
-    sd_total = 0
-    for value in values:
-        sd_total += square(value - avg)
-    sd = math.sqrt(sd_total / len(values))
-
-    sd_data[ft] = sd
-    averaged_data[ft] = avg
-
-    filtered_values = [x for x in values if (x > avg - (2*sd) and x < avg + (2*sd))]
-    sd_total = 0
-    filtered_avg = sum(filtered_values)/len(filtered_values)
-    for value in filtered_values:
-        sd_total += square(value - filtered_avg)
-    post_filter_sd_data[ft] = math.sqrt(sd_total / len(filtered_values))
-    post_filter_averaged_data[ft] = filtered_avg
-
-medians = dict([(ft, sorted(values)[len(values)/2]) for ft, values in strength_data.items()])
-print(medians)
-
-#print("Averages: ")
-#print(averaged_data)
-#
-#print("Filtered averages: ")
-#print(post_filter_averaged_data)
-#
-#print("Standard deviations: ")
-#print(sd_data)
-#
-#print("Filtered standard deviations: ")
-#print(post_filter_sd_data)
-#
-#print("Num values: ")
-#print(dict([(key,len(vals)) for key,vals in strength_data.items()]))
+pprint.pprint(medians)
