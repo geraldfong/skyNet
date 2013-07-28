e53dc4f97703c37884a09c5e00dda72852f482a5
Achal Dave <achalddave@live.com>
2013-07-27 17:45:48 -0700

    wifi monitor app
    i should probably have made more incremental commits but hackathon

---
+++ WifiMonitor/src/com/skynet/wifimonitor/MainActivity.java
@@ -0,0 +1,92 @@
+package com.skynet.wifimonitor;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private static String TAG = "MainActivity";
+	private static boolean checkInBackground = false;
+
+	/* he's mine! */
+	public WifiManager myWifiMan;
+	private WifiReceiver receiver;
+	private Button requestSender;
+	private WebRequester myRequester;
+	private Thread scannerThread;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		myRequester = new WebRequester();
+		myWifiMan = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+		// but I don't want to share him!
+		receiver = new WifiReceiver(myWifiMan);
+
+		scannerThread = new WifiScannerThread();
+		registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));					
+		scannerThread.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onPause() {
+		if (!checkInBackground && scannerThread.isAlive()) {
+			Log.d(TAG, "Interrupting thread in onPause()");
+			scannerThread.interrupt();
+		}
+		super.onPause();
+	}
+	
+	@Override
+	public void onResume() {
+		if (!scannerThread.isAlive()) {
+			Log.d(TAG, "Starting thread in onResume()");
+			scannerThread = new WifiScannerThread();
+			scannerThread.start();
+		} else {
+			Log.d(TAG, "Thread was not interrupted");
+		}
+		super.onResume();
+	}
+	
+	@Override
+	public void onDestroy() {
+		if (scannerThread.isAlive()) {
+			Log.d(TAG, "Stopping thread in onDestroy()");
+			scannerThread.interrupt();
+		}
+		super.onDestroy();
+	}
+
+	private class WifiScannerThread extends Thread {
+		private static final int intervalMs = 500;
+		public void run() {
+			Log.d(TAG, "Started WifiScannerThread");
+			while (true) {
+				try {
+					Thread.sleep(intervalMs);
+				} catch (InterruptedException e) {
+					Log.d(TAG, "Stopped WifiScannerThread");
+					return;
+				}
+				myWifiMan.startScan();
+			}
+		}
+	}
+
+}
---
+++ WifiMonitor/src/com/skynet/wifimonitor/WifiReceiver.java
@@ -0,0 +1,56 @@
+package com.skynet.wifimonitor;
+
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+public class WifiReceiver extends BroadcastReceiver {
+	private static String TAG = "WifiReceiver";
+	private static String url = "http://requestb.in/sxr1q6sx";
+
+	// haha he's also mine!
+	private WifiManager myWifiMan;
+	private WebRequester myRequester;
+
+	public WifiReceiver(WifiManager wifiMan) {
+		myWifiMan = wifiMan;
+		myRequester = new WebRequester();
+	}
+	
+	@Override
+	public void onReceive(Context ctx, Intent intnt) {
+	/*						^ i hear no vowels is popular */
+		JSONObject data = new JSONObject();
+		Log.d(TAG, "Received data");
+
+		try {
+			// unix time
+			data.put("time", System.currentTimeMillis() / 1000L);
+	
+			List<ScanResult> theWifis = myWifiMan.getScanResults();
+			JSONArray wifiData = new JSONArray();
+			for (ScanResult wifi : theWifis) {
+				JSONObject wifiDatum = new JSONObject();
+				wifiDatum.put("ssid", wifi.SSID);
+				wifiDatum.put("strength", wifi.level);
+				wifiData.put(wifiDatum);
+			}
+			data.put("wifiData", wifiData);
+			myRequester.sendPost(url, data.toString());
+			Log.d(TAG, "Sending JSON Data: ");
+			Log.d(TAG, data.toString());
+		} catch (JSONException e) {
+			Log.e(TAG, "Couldn't put JSON data");
+			e.printStackTrace();
+		}
+	}
+}
