4b767aba6ff301a44672094b0317291e8fb46e17
Achal Dave <achalddave@live.com>
2013-07-28 05:01:11 -0700

    i dont know whats with this diff

--- data_parser.py
+++ data_parser.py
@@ -48,6 +48,12 @@ for line in open(data_path):
             ssid_datas[ssid_name] = SSIDData(ssid_name)
         ssid_datas[ssid_name].add_data(ft, strength_data['strength'])
 
-medians = dict([(name, data.get_median_strengths()) for name, data in ssid_datas.items()])
+medians = []
+for name, data in ssid_datas.items():
+    strength_to_feet = [(v, k) for k, v in data.get_median_strengths().items()]
+    print("ssid: " + name)
+    print("\n".join([str(-k)+" "+str(v) for (k,v) in strength_to_feet]))
+    medians.append((name, strength_to_feet))
+#medians = [(name, data.get_median_strengths().items()) for name, data in ssid_datas.items()]
 
 pprint.pprint(medians)
