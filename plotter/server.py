6f1156bb2c9fc328357a93910306967fa54cfc9e
Gerald Fong <geraldgfong@gmail.com>
2013-08-31 00:32:21 +0000

    Make flask publicly visible and make debug tree

--- plotter/server.py
+++ plotter/server.py
@@ -7,4 +7,6 @@ def hello_world():
   return 'Hello World!'
 
 if __name__ == '__main__':
-  app.run()
+  # Debug auto restarts server on code changes
+  # Setting host to 0.0.0.0 makes it externally visible through server
+  app.run(host='0.0.0.0', debug=True)
