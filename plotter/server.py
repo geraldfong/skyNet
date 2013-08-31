00773595e89b328b74abf3de5b63c29230836e02
Gerald Fong <geraldgfong@gmail.com>
2013-08-31 02:52:48 +0000

    Add a check for the right json parameters

--- plotter/server.py
+++ plotter/server.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return 'Hello World!\n'
 
 @app.route('/data', methods=['POST'])
 def hande_data():
   json_data = request.form['data']
   decoded = json.loads(json_data)
+
+  expected_keys = ('sensor_type', 'value', 'timestamp', 'experiment')
+  if any(key not in decoded for key in expected_keys):
+    return 'ERROR: Malformed json; json needs to have %s\n' % (' '.join(expected_keys))
+
   sensor_type = decoded['sensor_type']
   value = decoded['value']
   timestamp = decoded['timestamp']
@@ -26,7 +31,7 @@ def hande_data():
     'experiment': experiment
   }
   sensor_collection.insert(document_data) 
-  return "OK"
+  return "OK\n"
 
 if __name__ == '__main__':
   client = MongoClient()
