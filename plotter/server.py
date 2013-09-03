93476e20b703057c98449cc7fbee21060d933cf5
Gerald Fong <geraldgfong@gmail.com>
2013-09-03 23:32:19 +0000

    Added comments to explain each of the parameters in the /data method

--- plotter/server.py
+++ plotter/server.py
@@ -9,6 +9,11 @@ def handle_data():
   json_data = request.form['data']
   decoded = json.loads(json_data)
 
+  # sensor_type - the type of sensor, ie wifi, accelerometer, gyro, etc.
+  # sensor_id - the id of the specific sensor, ie x-direction, y-direction, SKYNET, skynet
+  # value - the data value of interest such as signal strength from wifi
+  # timestamp - a timestamp in seconds from unix
+  # experiment - hand picked experiment name which is later used to generate graphs with
   expected_keys = ('sensor_type', 'sensor_id', 'value', 'timestamp', 'experiment')
   if any(key not in decoded for key in expected_keys):
     return 'ERROR: Malformed json; json needs to have [%s]\n' % (' '.join(expected_keys))
