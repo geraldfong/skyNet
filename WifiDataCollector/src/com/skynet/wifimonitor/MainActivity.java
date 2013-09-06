ab6d926c10f26da950ca6d783abf957cd1ba8e2f
Gerald Fong <geraldgfong@gmail.com>
2013-09-05 20:09:14 -0700

    Used constants class instead of string literals; Added a BroadCaster receiver for when csan is finished

--- WifiDataCollector/src/com/skynet/wifimonitor/MainActivity.java
+++ WifiDataCollector/src/com/skynet/wifimonitor/MainActivity.java
@@ -2,10 +2,13 @@ package com.skynet.wifimonitor;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -14,38 +17,51 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 	private static String TAG = "MainActivity";
 
-	public WifiManager wifiManager;
-
 	private EditText experimentField;
 	private EditText numMinutesField;
 	private Button start;
-	
-	private String experiment;
-	private int numMin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
-		wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 		experimentField = (EditText) findViewById(R.id.experiment);
 		numMinutesField = (EditText) findViewById(R.id.numMinutes);
 		start = (Button) findViewById(R.id.start);
-		
+	
+		setReceivers();
 		setListeners();
+	}
+	
+	private void setReceivers() {
+		// This will re-enable the start button when we receive a finished BROADCAST_FINISHED message
+		IntentFilter finishedScanIntentFilter = new IntentFilter(Constants.BROADCAST_FINISHED);
+		BroadcastReceiver finishedScanReceiver = new BroadcastReceiver() {
+			public void onReceive(Context context, Intent intent) {
+				start.setText("Start");
+				start.setEnabled(true);
+			}
+		};
+		LocalBroadcastManager.getInstance(MainActivity.this).
+			registerReceiver(finishedScanReceiver, finishedScanIntentFilter);
 	}
 
 	private void setListeners() {
 		start.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				String experiment;
+				int numMin;
+				
+				// Try to parse the experiment field
 				try {
 					experiment = experimentField.getText().toString();
 				} catch (NumberFormatException e) {
 					new AlertDialog.Builder(MainActivity.this).setMessage("Couldn't parse the experiment name. Make sure it is a string.").show();
 					return;
 				}
+				// Try to parse the number of minutes field
 				try {
 					numMin = Integer.parseInt(numMinutesField.getText().toString());
 				} catch (NumberFormatException e) {
@@ -53,9 +69,18 @@ public class MainActivity extends Activity {
 					return;
 				}
 
+				// Create an intent to send over to WifiPollService
+				// An intent is an information package to send over to another entity
 				Intent wifiServiceIntent = new Intent(MainActivity.this, WifiPollService.class);
-				wifiServiceIntent.putExtra("numMin", numMin);
-				wifiServiceIntent.putExtra("experiment", experiment);
+				wifiServiceIntent.putExtra(Constants.NUM_MIN, numMin);
+				wifiServiceIntent.putExtra(Constants.EXPERIMENT, experiment);
+				
+				// Prevent the user from pressing the start button again.
+				// The start button will be re-enabled by the scanFinishedReceiver
+				start.setText("Running...");
+				start.setEnabled(false);
+				
+				// Begin the scanning service
 				MainActivity.this.startService(wifiServiceIntent);
 			}
 		});
@@ -67,5 +92,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
--- WifiDataCollector/src/com/skynet/wifimonitor/WifiPollService.java
+++ WifiDataCollector/src/com/skynet/wifimonitor/WifiPollService.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class WifiPollService extends IntentService {
@@ -13,12 +14,13 @@ public class WifiPollService extends IntentService {
 	private WifiManager wifiManager;
 	private WifiReceiver wifiReceiver;
 	
-	private static final int intervalMs = 500;
-	
 	public WifiPollService() {
 		super("Wifi Poll Service");
 	}
 	
+	/** Save a copy of the wifiManager after getting it from the application context. Also create
+	 *  a version of the wifiReceiver that will be used throughout this service. 
+	 */
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -26,20 +28,23 @@ public class WifiPollService extends IntentService {
 		wifiReceiver = new WifiReceiver(wifiManager);
 	}
 
+	/** Begin scanning in the background for a given number of minutes with an experiment name.
+	 */
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		int numMin = intent.getIntExtra("numMin", 1);
-		String experiment = intent.getStringExtra("experiment");
+		int numMin = intent.getIntExtra(Constants.NUM_MIN, Constants.DEFAULT_NUM_MIN);
+		String experiment = intent.getStringExtra(Constants.EXPERIMENT);
 		
 		wifiReceiver.setExperiment(experiment);
 		long curTime = System.currentTimeMillis()/1000L;
 		long endTime = curTime + numMin * 60;
 		
+		// Register receiver and enter scanning cycle.
 		registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 		Log.d(TAG, "Starting to scan");
 		while (System.currentTimeMillis()/1000L < endTime) {
 			try {
-				Thread.sleep(intervalMs);
+				Thread.sleep(Constants.SCAN_INTERVAL_MS);
 			} catch (InterruptedException e) {
 				Log.d(TAG, "Stopped WifiScannerThread");
 				return;
@@ -47,7 +52,11 @@ public class WifiPollService extends IntentService {
 			wifiManager.startScan();
 		}
 		Log.d(TAG, "Finished scanning");
-		unregisterReceiver(wifiReceiver);	
+		unregisterReceiver(wifiReceiver);
+		
+		// Create an intent to send data back to the original activity that we have finished the task
+		Intent finishIntent = new Intent(Constants.BROADCAST_FINISHED);
+		LocalBroadcastManager.getInstance(this).sendBroadcast(finishIntent);
 	}
 	
 	
