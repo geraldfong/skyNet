0234a10c8ce0bddd0506197d2bf3b4a29f69785b
Gerald Fong <geraldgfong@gmail.com>
2013-09-13 18:29:15 -0700

    Remove excess import

--- WifiDataCollector/src/com/skynet/wifimonitor/MainActivity.java
+++ WifiDataCollector/src/com/skynet/wifimonitor/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.Menu;
